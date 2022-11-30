085dedab2828ef4f3ba454ce0e77c50f410d6176
Dario Cubero <cubero@dxc.com>
2022-11-30 02:27:22 +0100

    Mostrar sitios por categoria, maquetacion tabla, botones con href pendientes

--- www/js/script.js
+++ www/js/script.js
@@ -1,10 +1,24 @@
 getCategories();
 
-$(document).ready(function() {
-    $("#categorias li").click(function(event) {
-        alert(event.target.id);
-    });
-});
+// $(document).ready(function() {
+// document.querySelector("#categorias li").click(function(event) {
+//   console.log(":D");
+//     alert(event.target.id);
+// });
+// });
+
+// document.querySelectorAll("#categorias li").forEach(x => x.addEventListener("click", hellou);
+
+// function hellou(){
+//   alert("XD");
+// }
+
+// document.querySelector("#categorias li").click(function(event) {
+//   // .addEventListener("click", myFunction);
+
+//   // console.log(":D");
+//     // alert(event.target.id);
+// });
 
 // START
 function getCategories() {
@@ -15,7 +29,9 @@ function getCategories() {
       let child = document.createElement("li");
       child.innerText = category.name;
       child.id = category.id;
-    //   child.setAttribute("onclick", `clickCategory(${category.id})`);
+      child.addEventListener("click", function (event) {
+        getSitesByCategory(category.id);
+      });
       parent.appendChild(child);
     });
   };
@@ -24,18 +40,85 @@ function getCategories() {
     .then((data) => busquedaCategorias(data));
 }
 
-function getSitesByCategory(id) {
-    alert(id + "hola");
-//   let busquedaSites = (data) => {
-//     data.forEach((category) => {
-//       let parent = document.getElementsByTagName("p")[0];
-//       let child = document.createElement("li");
-//       child.innerText = category.name;
-//       parent.appendChild(child);
-//     });
-//   };
+// function addRow(tableID) {
+//   // Get a reference to the table
+//   let tableRef = document.getElementById(tableID);
+//   // Insert a row at the end of the table
+//   let newRow = tableRef.insertRow(-1);
+//   // Insert a cell in the row at index 0
+//   let newCell = newRow.insertCell(0);
+//   // Append a text node to the cell
+//   let newText = document.createTextNode('New bottom row');
+//   newCell.appendChild(newText);
+// }
 
-//   fetch("http://localhost:3000/categories/"+ id)
-//     .then((res) => res.json())
-//     .then((data) => busquedaSites(data));
+function limpiarTabla() {
+  document
+    .querySelectorAll("#tabla-sitios tr:not(.rowthead)")
+    .forEach((x) => x.remove());
+}
+
+function getSitesByCategory(id) {
+  limpiarTabla();
+  //devuelve los sitios de una categoria
+  let busquedaSitesByCategory = (data) => {
+    data.forEach((category) => {
+      let tablitaBonita = document.getElementById("tabla-sitios");
+
+      // sitio
+      let newRow = tablitaBonita.insertRow(-1); // Insert a row at the end of the table
+      let newCell0 = newRow.insertCell(); // Insert a cell in the row at index 0
+      let newText = document.createTextNode(category.name); // Append a text node to the cell
+      newCell0.appendChild(newText);
+
+      // usuario
+      let newCell1 = newRow.insertCell();
+      newCell1.classList.add("align-middle");
+      let newText1 = document.createTextNode(category.user);
+      newCell1.appendChild(newText1);
+
+      // fecha
+      let newCell2 = newRow.insertCell();
+      newCell2.classList.add("align-middle");
+      let newText2 = document.createTextNode(category.createdAt);
+      newCell2.appendChild(newText2);
+
+      // icons
+      let newCell3 = newRow.insertCell();
+      newCell3.classList.add("align-middle");
+
+      //button open
+      let rem = document.createElement("button");
+      rem.onclick = function () {
+        alert("1");
+      };
+      rem.innerHTML = '<i class="fa-solid fa-folder fa-beat"> </i>';
+      newCell3.appendChild(rem);
+
+      //button eliminar
+      rem = document.createElement("button");
+      rem.onclick = function () {
+        alert("2");
+      };
+      rem.innerHTML = '<i class="fa-regular fa-trash-can fa-beat"></i>';
+      newCell3.appendChild(rem);
+
+      //button editar
+      rem = document.createElement("button");
+      rem.onclick = function () {
+        alert("3");
+      };
+      rem.innerHTML = '<i class="fa-solid fa-pen-to-square fa-beat"></i>';
+      newCell3.appendChild(rem);
+
+      // category.name; category.user; category.createdAt;
+      // let div = document.createElement("div");
+      // div.innerHTML = category.name;
+      // laOStia.appendChild(div);
+    });
+  };
+
+  fetch("http://localhost:3000/categories/" + id)
+    .then((res) => res.json())
+    .then((data) => busquedaSitesByCategory(data));
 }
