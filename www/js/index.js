2c45124119b50079e714c9fb73836b3250ba209d
Dario Cubero <cubero@dxc.com>
2022-12-09 13:09:15 +0100

    Cambio eventListeners click

--- www/js/index.js
+++ www/js/index.js
@@ -1,105 +1,101 @@
 // Cargamos las categorías
 $(document).ready(function () {
   getCategories();
-});
 
-function limpiarTabla() {
-  $("#tbodyid").empty();
-}
+  function limpiarTabla() {
+    $("#tbodyid").empty();
+  }
 
-// START
-function getCategories() {
-  // GET categorias
-  let busquedaCategorias = (data) => {
-    data.forEach((category) => {
-      let parent = document.getElementById("categorias");
-      let button = document.createElement("button");
-      button.classList.add("list-group-item", "list-group-item-action");
-      button.innerText = category.name;
-      // button.data-bs-toggle="list";
-      button.setAttribute("data-bs-toggle", "list");
-      button.type = "button";
-      button.id = category.id; //Añade el ID en cada elemento del LI.
-      button.addEventListener("click", function (event) {
-        getSitesByCategory(category.id);
-        // setHrefCreateSite(category.id); //Modificamos el href del botón "createSite" cuando esté clickada una categoría
+  // START
+  function getCategories() {
+    // GET categorias
+    let busquedaCategorias = (data) => {
+      data.forEach((category) => {
+        let parent = document.getElementById("categorias");
+        let button = document.createElement("button");
+        button.classList.add("list-group-item", "list-group-item-action");
+        button.innerText = category.name;
+        // button.data-bs-toggle="list";
+        button.setAttribute("data-bs-toggle", "list");
+        button.type = "button";
+        button.id = category.id; //Añade el ID en cada elemento del LI.
+        button.addEventListener("click", function (event) {
+          getSitesByCategory(category.id);
+          // setHrefCreateSite(category.id); //Modificamos el href del botón "createSite" cuando esté clickada una categoría
+        });
+        parent.appendChild(button);
       });
-      parent.appendChild(button);
-    });
-  };
-  fetch("http://localhost:3000/categories")
-    .then((res) => res.json())
-    .then((data) => busquedaCategorias(data));
-}
+    };
+    fetch("http://localhost:3000/categories")
+      .then((res) => res.json())
+      .then((data) => busquedaCategorias(data));
+  }
 
-function getSitesByCategory(id) {
-  limpiarTabla();
-  //devuelve los sitios de una categoria
+  function getSitesByCategory(id) {
+    limpiarTabla();
+    //devuelve los sitios de una categoria
 
-  let parent = document.getElementsByTagName("tbody")[0];
-  parent.innerHTML = "";
+    let parent = document.getElementsByTagName("tbody")[0];
+    parent.innerHTML = "";
 
-  // Creamos la tabla de sitios
-  let busquedaSitesByCategory = (data) => {
-    
-    data.forEach((sites) => {
-      let tr = document.createElement("tr"); //Creamos una fila por cada 'Site'
-      let td = document.createElement("td"); //y vamos creando las columnas...
+    // Creamos la tabla de sitios
+    let busquedaSitesByCategory = (data) => {
+      data.forEach((sites) => {
+        let tr = document.createElement("tr"); //Creamos una fila por cada 'Site'
+        let td = document.createElement("td"); //y vamos creando las columnas...
 
-      td.innerText = sites.name;
-      tr.appendChild(td);
+        td.innerText = sites.name;
+        tr.appendChild(td);
 
-      //User
-      td = document.createElement("td");
-      td.innerText = sites.user;
-      tr.appendChild(td);
+        //User
+        td = document.createElement("td");
+        td.innerText = sites.user;
+        tr.appendChild(td);
 
-      //Created At
-      td = document.createElement("td");
-      td.innerText = sites.createdAt;
-      tr.appendChild(td);
+        //Created At
+        td = document.createElement("td");
+        td.innerText = sites.createdAt;
+        tr.appendChild(td);
 
-      // Icons PENDING
-      td = document.createElement("td");
-      let boton = document.createElement("button");
+        // Icons PENDING
+        td = document.createElement("td");
+        let boton = document.createElement("button");
 
-      // abrir
-      boton.onclick = function () {
-        alert("1");
-      };
-      boton.innerHTML = '<i class="fa-solid fa-folder fa-beat"> </i>';
-      td.appendChild(boton);
+        // abrir
+        boton.onclick = function () {
+          alert("1");
+        };
+        boton.innerHTML = '<i class="fa-solid fa-folder fa-beat"> </i>';
+        td.appendChild(boton);
 
-      // eliminar
-      boton = document.createElement("button");
-      boton.onclick = function () {
-        alert("2");
-      };
-      boton.innerHTML = '<i class="fa-regular fa-trash-can fa-beat"></i>';
-      td.appendChild(boton);
+        // eliminar
+        boton = document.createElement("button");
+        boton.onclick = function () {
+          alert("2");
+        };
+        boton.innerHTML = '<i class="fa-regular fa-trash-can fa-beat"></i>';
+        td.appendChild(boton);
 
-      // editar
-      boton = document.createElement("button");
-      boton.onclick = function () {
-        alert("1");
-      };
-      boton.innerHTML = '<i class="fa-solid fa-pen-to-square fa-beat"></i>';
-      td.appendChild(boton);
+        // editar
+        boton = document.createElement("button");
+        boton.onclick = function () {
+          alert("1");
+        };
+        boton.innerHTML = '<i class="fa-solid fa-pen-to-square fa-beat"></i>';
+        td.appendChild(boton);
 
-      tr.appendChild(td); //Insertamos columnas en la fila
-      parent.appendChild(tr); //Añadimos la fila al tbody
-    });
-  };
+        tr.appendChild(td); //Insertamos columnas en la fila
+        parent.appendChild(tr); //Añadimos la fila al tbody
+      });
+    };
 
-  fetch("http://localhost:3000/categories/" + id) // Sitios de una categoría
-    .then((res) => res.json())
-    .then((data) => busquedaSitesByCategory(data));
-}
+    fetch("http://localhost:3000/categories/" + id) // Sitios de una categoría
+      .then((res) => res.json())
+      .then((data) => busquedaSitesByCategory(data));
+  }
 
-// function setHrefCreateSite(id) {
-//   let boton = document.getElementById("buttonSite");
-//   boton.href = "createSite.html?site=" + id;
-// }
-
-
-
+  // function setHrefCreateSite(id) {
+  //   let boton = document.getElementById("buttonSite");
+  //   boton.href = "createSite.html?site=" + id;
+  // }
+});
--- www/js/sites.js
+++ www/js/sites.js
@@ -1,63 +1,57 @@
-function getParams() {
-  if (hasQueryParams()) {
-    let urlParams = new URLSearchParams(window.location.search);
-    let idSite = urlParams.get("site");
-    alert("Parámetro recogido de la URL: " + idSite);
-  }
-}
-
-function hasQueryParams(url) {
-  return window.location.href.indexOf("?") > -1;
-}
-
-function generar() {
-  let long = 8;
-  let caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ2346789";
-  let contraseña = "";
-  for (let i = 0; i < long; i++) contraseña += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
-  document.getElementById("pass").value = contraseña;
-}
-
 $(document).ready(function () {
   getParams();
-});
-
-window.onload = function (e) {
-  console.log("documento cargado");
 
   let valor = "";
   document.getElementById("pass").value = valor;
-};
 
-
-function guardarSite() {
-  let site = document.getElementById("sitio").value;
-  let url = document.getElementById("url").value;
-  let user = document.getElementById("user").value;
-  let pass = document.getElementById("pass").value;
-  let desc = document.getElementById("desc").value;
-
-  let sitio = new Object();
-  sitio.name = site;
-  sitio.url = url;
-  sitio.user = user;
-  sitio.password = pass;
-  sitio.description = desc;
-
-  let jsonString = JSON.stringify(sitio);
-  console.log(jsonString);
-
-  let url2 = "http://localhost:3000/categories/:id'";
-
-  async function makePostRequest(url2, requestType) {
-    await fetch(url2, {
-      method: requestType,
-      body: JSON.stringify(jsonString),
-    }).then(async (rawResponse) => {
-      let content = await rawResponse.json();
-      console.log(content);
-    });
+  function getParams() {
+    if (hasQueryParams()) {
+      let urlParams = new URLSearchParams(window.location.search);
+      let idSite = urlParams.get("site");
+      // alert("Parámetro recogido de la URL: " + idSite);
+    }
   }
 
-  makePostRequest(url2, "POST");
-}
+  function hasQueryParams(url) {
+    return window.location.href.indexOf("?") > -1;
+  }
+
+  document.getElementById("keyrandom").addEventListener("click", function () {
+    let long = 8;
+    let caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ2346789";
+    let contraseña = "";
+    for (let i = 0; i < long; i++) contraseña += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    document.getElementById("pass").value = contraseña;
+  });
+  
+  document.getElementById("botonGuardarSite").addEventListener("click", function () {
+    let site = document.getElementById("sitio").value;
+    let url = document.getElementById("url").value;
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+    let desc = document.getElementById("desc").value;
+
+    let sitio = new Object();
+    sitio.name = site;
+    sitio.url = url;
+    sitio.user = user;
+    sitio.password = pass;
+    sitio.description = desc;
+
+    let jsonString = JSON.stringify(sitio);
+    console.log(jsonString);
+
+    let url2 = "http://localhost:3000/categories/:id'";
+
+    async function makePostRequest(url2, requestType) {
+      await fetch(url2, {
+        method: requestType,
+        body: JSON.stringify(jsonString),
+      }).then(async (rawResponse) => {
+        let content = await rawResponse.json();
+        console.log(content);
+      });
+    }
+    makePostRequest(url2, "POST");
+  });
+});
